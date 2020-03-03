62f4476dfefbb458024f0ad9cf350be59a5332e1
rajpranz <rajeshwarrprasad@yahoo.com>
2020-03-04 04:52:47 +0530

    Setup app structure

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+const Header = () => {
+    return (
+        <div>
+        <Navbar color="success" light>
+          <NavbarBrand href="/" className="mr-auto"><span style={{color:'white'}}>DinnerBook</span></NavbarBrand>
+          
+        </Navbar>
+      </div>
+    );
+}
+
+
+export default Header;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from 'reactstrap';
+
+const Main = () => {
+  return (
+    <div>
+      <br/>
+      <h3>Whats for dinner tonight?</h3>
+      <br/>
+      <Button outline color="success" size="lg">Find Inspiration</Button>{' '}
+    </div>
+  );
+};
+
+export default Main;
