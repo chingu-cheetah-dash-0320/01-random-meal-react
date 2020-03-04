bae95ad4f3e23ef7ae2eca11537b884504fe4f78
rajpranz <rajeshwarrprasad@yahoo.com>
2020-03-04 06:26:48 +0530

    Create responsive layout

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
+import {RecipieDetails} from "./RecipieDetails";
+
+var mainStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+}
 
 const Main = () => {
   return (
-    <div>
-      <br/>
-      <h3>Whats for dinner tonight?</h3>
-      <br/>
-      <Button outline color="success" size="lg">Find Inspiration</Button>{' '}
+    <div className="wrapper">
+      <br />
+      <h3 style={{textAlign: 'center'}}>Whats for dinner tonight?</h3>
+      <br />
+      <Button outline color="success" block size="lg" >
+        Find Inspiration
+      </Button>{" "}
+      <br />
+      <RecipieDetails/>
     </div>
   );
 };
---
+++ src/components/RecipieDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function RecipieDetails() {
+  return (
+    <div>
+      <div className="flex-container wrapper">
+        <aside className="sidebar">Recipie Description ...</aside>
+
+        <section className="content">Recipie Steps ...</section>
+      </div>
+      <div className="flex-container wrapper">
+        <footer className="footer">Video goes here</footer>
+      </div>
+    </div>
+  );
+}
